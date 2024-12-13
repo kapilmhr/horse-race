f91f23234d87a22439e01575c5492235bca7a1d8
Kapil <kapilmhr016@gmail.com>
2024-12-13 16:48:54 +1100

    Initial Commit

---
+++ horse_racing/horse_racingApp.swift
@@ -0,0 +1,17 @@
+//
+//  horse_racingApp.swift
+//  horse_racing
+//
+//  Created by Kapil Maharjan on 13/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct horse_racingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ horse_racingTests/horse_racingTests.swift
@@ -0,0 +1,17 @@
+//
+//  horse_racingTests.swift
+//  horse_racingTests
+//
+//  Created by Kapil Maharjan on 13/12/2024.
+//
+
+import Testing
+@testable import horse_racing
+
+struct horse_racingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
